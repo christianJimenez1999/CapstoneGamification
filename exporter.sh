1fe52e1f37b0c2be552d2138a0b27befbb25bc0e
Ubuntu <ubuntu@ip-172-31-5-180.ec2.internal>
2021-03-12 21:55:53 +0000

    add exporter file

---
+++ exporter.sh
@@ -0,0 +1,15 @@
+export HOST="localhost"
+export USERNAME="Geo"
+export PASSWORD="Geo"
+export DATABASE="cap_info"
+
+# Geo
+# is user and password
+
+# cap_info
+# is the database name
+
+# mysql -p -u Geo cap_info < sql/tables.sql
+# how to make sql process a file
+
+# npm install --save mysql
