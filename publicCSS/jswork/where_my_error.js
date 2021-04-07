888115a08510ad825c3f00ca2b37caeb4a06bca1
quintero <quinterojosuej@gmail.com>
2021-04-07 07:34:10 +0000

    more of the project!

--- publicCSS/jswork/where_my_error.js
+++ publicCSS/jswork/where_my_error.js
@@ -1,13 +1,14 @@
 console.log("I EXISTS :D");
 
-var points = 0
+var correct = 0
+var gotten = 0
 
 var lives = 4
 
-var currentQuestion = 1
+var currentQuestion = 0
 var tempQuestion = 0
 
-var difficulty = "mid"
+var difficulty = "start";
 
 var answer = 0
 
@@ -17,31 +18,55 @@ function controller() { // check difficulty and the
     $(document).ready(function() {
         console.log("COURAGE THE COWARDLY DOG SAYS: YAY!")
     
-    if(lives <= 0) {
-        console.log("OOF")
+    
+    if(difficulty="Syntax"){ // only progress on the complete cycle
+        currentQuestion += 1;
     }
-    currentQuestion += 1;
     
-    tempQuestion = currentQuestion;
+    difficulty = pointing[difficulty] // move to the other one
+    // $("#type").html(difficulty)
     
-    canClick = true
+    if(currentQuestion > 5) { // don't go over
+        currentQuestion = 1
+    }
     
-    maker();
+    if(Object.keys(questions[difficulty][currentQuestion]).length == 0){ // out of questions here
+        finalize()
+        return;
+    }
+    else if(lives <= 0) { // out of lives here
+        console.log("OOF");
+        finalize()
+        return;
+    }
+    else{
     
-    // if the the tempQuestion equals the the internalBotCount then call the checker, else don't
-            setTimeout(
-              function() 
-              {
-                  if(tempQuestion == currentQuestion){
-                      checkAnswer();
-                  }
-                  else{
-                      console.log("NO TIMEOUT!");
-                  }
-              }, 3050); // this is a little more than 3.5 seconds
+        tempQuestion = currentQuestion;
+        
+        canClick = true;
+        
+        maker();
+        
+        // if the the tempQuestion equals the the internalBotCount then call the checker, else don't
+        setTimeout(
+          function() 
+          {
+              if(tempQuestion == currentQuestion){
+                  checkAnswer();
+              }
+              else{
+                  console.log("NO TIMEOUT!");
+              }
+          }, 5050); // this is a little more than 5 seconds
+        
+    }
     
     });
 };
+
+function finalize() {
+    console.log("THE END")
+}
 
 function maker() {
     
@@ -87,11 +112,19 @@ function checkAnswer() { // implement the actual checking stuff
     
     if(answer == questions[difficulty][currentQuestion]["correct"]){
         console.log("KATS SAYS: ssassy success")
-        points += 1;
+        correct += 1;
+        $("#correct").html(correct)
     }
     else{
         console.log("KATS SAYS: failure doggy")
+        lives -= 1;
+        $("#lives").html(lives)
     }
+    gotten += 1
+    $("#gotten").html(gotten)
+    
+    
+    delete questions[difficulty][currentQuestion]; // get rid of the question
     
     controller()
     
@@ -119,22 +152,60 @@ function newQuestion(question, inputs) {
 
 // for tabbing use &nbsp;&nbsp;
 // for the <> use the &lt; &gt;
-var questions = {
-    mid: {
+
+var questions = { // questions go log -> run -> syn -> log 
+    Logical:{
+        1:{
+            question: "Hello!",
+            lines: ["1 #include &lt;iostream&gt;", "2 using namespace std;", "3 ", "4 int main() {", "5 &nbsp;&nbsp; string my_string = \"Hello!\";",
+            "6 ", "7&nbsp;&nbsp; for(int var = 0; var < my_string.size(); var++);", "8&nbsp;&nbsp;&nbsp;&nbsp; cout << my_string;", "9 }"],
+            correct: 8
+        }
+    },
+    Syntax: {
+        1:{
+            question: "error: assigning to 'int' from incompatible type 'int *'",
+            lines: ["1 #include &lt;iostream&gt;", "2 using namespace std;", "3 ", "4 using namespace std;", "5 int main() {", "6&nbsp;&nbsp; int* p1, p2;", "7&nbsp;&nbsp; n=20;",
+            "8&nbsp;&nbsp; p1 = &n;","9&nbsp;&nbsp; p2=&n", "10}"],
+            correct: 9
+        },
         2:{
             question: "error: invalid conversion from ‘const char*’ to ‘int’",
-            lines: ["1 #include <string>", "2 using namespace std;", "3 int main() {", "4&nbsp;&nbsp; int num_items = '1';", "5&nbsp;&nbsp; cout << num_items;","6 }"],
+            lines: ["1 #include &lt;iostream&gt;", "2 using namespace std;", "3 int main() {", "4&nbsp;&nbsp; int num_items = '1';", "5&nbsp;&nbsp; cout << num_items;","6 }"],
             correct: 4
         },
         3:{
             question: "error: use of undeclared identifier 'iter'",
-            lines:["1 #include <iostream>", "2 #include <vector>", "3 using namespace std;", "4 ", "5 int main () {", "6&nbsp;&nbsp; vector&lt;int&gt; my_list;", "7 ", 
+            lines: ["1 #include &lt;iostream&gt;", "2 #include &lt;vector&gt;", "3 using namespace std;", "4 ", "5 int main () {", "6&nbsp;&nbsp; vector&lt;int&gt; my_list;", "7 ", 
             "8&nbsp;&nbsp; for(int iter = 0; iter < 20; iter++){", "9&nbsp;&nbsp;&nbsp;&nbsp; my_list.push_back(iter);", "10&nbsp;&nbsp; }", "11&nbsp;&nbsp; cout << iter << endl;", "12 }"],
             correct: 11
         },
         4:{
-            
+            question: "error: linker command failed with exit code 1",
+            lines: ["1 #include &lt;iostream&gt;", "2 #include &lt;string&gt;", "3 int Main() {", "4 ", "5&nbsp;&nbsp; int a=4, b=6, c=10", "6&nbsp;&nbsp; printf(\"\nThe addition of %d and %d is = %d\",a,b,c);",
+            "7 }"],
+            correct: 3
+        },
+        5:{
+            question: "error: expected ';' at end of declaration",
+            lines: ["1 #include &lt;iostream&gt;", "2 #include &lt;string>&gt;", "3 using namespace std;", "4 const int iter = 0;", "5 ", "6 int main () {", "7 &nbsp;&nbsp; string my_string=\"myString;\"",
+            "8 &nbsp;&nbsp; for(int iter = 0; iter < my_string.length(); iter++) {", "9&nbsp;&nbsp;&nbsp;&nbsp; cout << my_string[iter] << endl;", "10&nbsp;&nbsp; }",
+            "11 }"],
+            correct: 7
+        },
+        6:{
+            question: "error: extraneous closing brace ('}')",
+            lines: ["1 #include &lt;iostream&gt;", "2 #include &lt;string&gt;", "3 using namespace std;", "4 int main() {", "5&nbsp;&nbsp; string my_string = \"myString;\";",
+            "6&nbsp;&nbsp; for(int iter = 0; iter < my_string.length(); iter++)", "7&nbsp;&nbsp;&nbsp;&nbsp; cout << my_string[iter] << endl;", "8&nbsp;&nbsp; }", "9 }"],
+            correct:6
         }
         
     }
+};
+
+let pointing={
+    start:"Logical",
+    Logical:"Run_time",
+    Run_time:"Syntax",
+    Syntax:"Logical"
 }
