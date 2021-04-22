27c391cc7eee46d1ce033ca1ecdca1a7e7162151
Ubuntu <ubuntu@ip-172-31-5-180.ec2.internal>
2021-04-22 22:50:09 +0000

    update categories js file

--- publicCSS/jswork/categories.js
+++ publicCSS/jswork/categories.js
@@ -2,7 +2,6 @@ var attempts = 4;
 var currentQuestion = 0;
 var currentLevel = 1;
 var streak = 0;
-var internalBotCount = 0;
 var difficulty = "intro";
 
 function createQuestion(stuff) {
@@ -35,6 +34,7 @@ function checkInputs() { // we know what to check based on the internalHumanCoun
         //currentQuestion += 1;
         if($("#categories_game").find("#inputCorrect").val() == questions[difficulty][currentQuestion]['correct']){
             console.log("This worked!");
+            currentLevel += 1;
         } else {
             console.log("You failed");
             attempts -= 1;
@@ -59,7 +59,7 @@ function maker() {
 +questions[difficulty][currentQuestion]['choices'][i+1]+"</h3>";
         } 
         
-        var humanInputA = "<div style='width:20%; margin:auto;'><h3>"
+        var humanInputA = "<div style='width:50%; margin:auto;'><h3>"
         
         var humanInputAA = "</h3><br>"
         var humanInputB = "<div id='parentCorrect'><input id=inputCorrect></div>"
@@ -77,41 +77,51 @@ function getStarted() {
     $(document).ready(function() {
         currentQuestion += 1;
         
-        if(currentQuestion > 8){
-            currentQuestion = 1
+        if(currentQuestion > 3){
+            currentQuestion = 1;
         }
-        var tempQuestion = internalBotCount;
-        
-        if(attempts == 0){
-            console.log("You Lose");
-        }
+        var tempQuestion = currentQuestion;
+        console.log(tempQuestion)
         
         if(currentLevel >= level_distribution[difficulty]) { // update difficulty
             if(difficulty == "intro") {
                 difficulty = "mid";
+                currentQuestion = 1;
                 $('#difficulty').hide().html("<h3 style='background-image: linear-gradient(to right, yellow , red); height: 100%'>mid</h3>").fadeIn('slow')
             }
             else{
                 difficulty = "hard";
+                currentQuestion = 1;
                 $('#difficulty').html("<h3 style='background-image: linear-gradient(to right, pink , purple); height: 100%'>mid</h3>")
             }
         }
         
-        maker(); 
+        if(difficulty == "hard" && Object.keys(questions[difficulty]).length == 0){ // they 100% the game, congrats
+            console.log("you're all done");
+        }
+        else if(Object.keys(questions[difficulty]).length == 0) {
+            console.log("You finished");
+        }
+        else if(attempts == 0) {
+            console.log("you failed");
+        }
+        else{
+            
+            maker(); 
             // implement the timer here, put a gif on the actual html 
             
             // if the the tempQuestion equals the the internalBotCount then call the checker, else don't
-            
             setTimeout(
               function() 
               {
-                  if(tempQuestion == internalBotCount){
+                  if(tempQuestion == currentQuestion){
                       checkInputs();
                   }
                   else{
                       console.log("NO TIMEOUT!");
                   }
               }, 30050); // this is a little more than 30 seconds
+        }
     });
 }
 
@@ -125,31 +135,95 @@ var questions = {
             3: "string",
             4: "double",
         },
-        correct: "int",
+        correct: "B",
     },
     2:{
-        question: "What is?",
+        question: "What is a class?",
+        choices:{
+            1: "A user defined data type",
+            2: "A variable type",
+            3: "Something1",
+            4: "Something2",
+        },
+        correct: "A",
+    },
+    3:{
+        question: "What goes inside the parenthesis of a typical for loop?",
+        choices:{
+            1: "A declared varible",
+            2: "A condition",
+            3: "A varible being changed",
+            4: "All of the above",
+        },
+        correct: "D", 
+    },
+    
+}, mid:{
+    1:{
+        question: "2What data type is used for single digit numbers?",
         choices:{
             1: "char",
             2: "int",
             3: "string",
             4: "double",
         },
-        correct: "char",
+        correct: "B",
     },
-    
-}, mid:{
-    1:{
-        
+    2:{
+        question: "2What is a class?",
+        choices:{
+            1: "A user defined data type",
+            2: "A variable type",
+            3: "Something1",
+            4: "Something2",
+        },
+        correct: "A",
+    },
+    3:{
+        question: "2What goes inside the parenthesis of a typical for loop?",
+        choices:{
+            1: "A declared varible",
+            2: "A condition",
+            3: "A varible being changed",
+            4: "All of the above",
+        },
+        correct: "D", 
     },
 }, hard:{
     1:{
-        
+        question: "3What data type is used for single digit numbers?",
+        choices:{
+            1: "char",
+            2: "int",
+            3: "string",
+            4: "double",
+        },
+        correct: "B",
+    },
+    2:{
+        question: "3What is a class?",
+        choices:{
+            1: "A user defined data type",
+            2: "A variable type",
+            3: "Something1",
+            4: "Something2",
+        },
+        correct: "A",
+    },
+    3:{
+        question: "3What goes inside the parenthesis of a typical for loop?",
+        choices:{
+            1: "A declared varible",
+            2: "A condition",
+            3: "A varible being changed",
+            4: "All of the above",
+        },
+        correct: "D", 
     }
 }
 };
 
 var level_distribution = { // basically to see how many points before a level up
-    intro: 7, // I just made them up
-    mid: 15 
+    intro: 3, // I just made them up
+    mid: 5 
 }
